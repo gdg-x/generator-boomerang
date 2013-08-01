6d550f3004938d640b63e94e902604d154ac91d1
Joshua Woodward <jwwoodward@ebay.com>
2013-08-01 12:52:09 -0700

    instructions
    guide on how to use the generator

---
+++ scripts/8687e597.scripts.js
@@ -0,0 +1,1 @@
+"use strict";angular.module("generatorBoomerangApp",[]).config(["$routeProvider",function(a){a.when("/",{templateUrl:"views/main.html",controller:"MainCtrl"}).otherwise({redirectTo:"/"})}]),angular.module("generatorBoomerangApp").controller("MainCtrl",["$scope",function(a){a.awesomeThings=["HTML5 Boilerplate","AngularJS","Karma"]}]);
