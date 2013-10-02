7eefa3e08902bd8b41dcd9f17c1ca02a31faea6c
James Williams <James.L.Williams@gmail.com>
2013-10-01 22:36:17 -0700

    grunt build now creating dist folder
    Live reload seems to be working.
    grunt server working as well.

--- app/templates/Gruntfile.js
+++ app/templates/Gruntfile.js
@@ -5,57 +5,73 @@ var moment = require('moment');
 var LIVERELOAD_PORT = 35729;
 var lrSnippet = require('connect-livereload')({port: LIVERELOAD_PORT});
 var mountFolder = function (connect, dir) {
-  return connect.static(require('path').resolve(dir));
+    return connect.static(require('path').resolve(dir));
 };
 
 module.exports = function (grunt) {
-  // load all grunt tasks
-  require('matchdep').filterDev('grunt-*').forEach(grunt.loadNpmTasks);
+    // load all grunt tasks
+    require('matchdep').filterDev('grunt-*').forEach(grunt.loadNpmTasks);
 
-  grunt.initConfig({
-    watch: {
-      options: {
-        nospawn: true,
-        livereload: LIVERELOAD_PORT
-      },
-      livereload: {
-        files: [
-          'index.html',
-          'js/*.js',
-            'css/*.css'
-        ],
-        tasks: ['build']
-      }
-    },
-    connect: {
-      options: {
-        port: 9000,
-        // change this to '0.0.0.0' to access the server from outside
-        hostname: 'localhost'
-      },
-      livereload: {
-        options: {
-          middleware: function (connect) {
-            return [
-              lrSnippet,
-              mountFolder(connect, '.')
-            ];
-          }
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        concat: {
+            options: {
+                separtor: ';'
+            },
+            dist: {
+                src: ['lib/**/*.js', 'js/**/*.js'],
+                dest: 'dist/js/boomerang.js'
+            }
+        },
+        uglify: {
+            options: {
+                banner: ""
+            },
+            dist: {
+                files: {
+
+                }
+            }
+        },
+        copy: {
+            main: {
+                files: [
+                    {expand: true, src: [
+                        'css/*.css', 'images/*', 'views/*', 'view/*', 'index.html'], dest: 'dist'}
+                ]
+            }
+        },
+        watch: {
+            all: {
+                files: ["css/**/*", "views/**/*", "js/**/*"],
+                tasks: ["default"],
+                options: {
+                    nospawn: true,
+                    interrupt: false,
+                    debounceDelay: 250
+                }
+            }
+        },
+        reload: {
+            port: 35729,
+            liveReload: {},
+            proxy: {
+                host: "localhost",
+                port: 8080
+            }
+        },
+        connect: {
+            server: {
+                options: {
+                    port: 9001,
+                    base: 'dist'
+                }
+            }
         }
-      }
-    },
-    open: {
-      server: {
-        path: 'http://localhost:<%%= connect.options.port %>'
-      }
-    }
-  });
+    });
 
-  grunt.registerTask('server', ['build', 'connect:livereload', 'open', 'watch']);
+    grunt.registerTask('server', ['build', 'connect', 'watch']);
 
-  grunt.registerTask('build', 'Build boomerang.', function () {
-    var unslug = function (slug) {
-      return slug.replace(/(-(\w))/g, function () { return ' ' + arguments[2].toUpperCase(); });
-    };
-  });
+    grunt.registerTask('build', ['concat', 'uglify', 'copy']);
+    grunt.registerTask('default', ['build']);
 };
